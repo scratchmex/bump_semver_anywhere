e427123cd251aa5f98ca1ece26a56bb3c78764fb
Ivan Gonzalez <scratchmex@gmail.com>
2021-10-31 17:32:13 -0600

    ci(gh-action): add entrypoint with git user configure

---
+++ gh-action/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# configure gh-action identity in git
+git config --global user.name "github-actions[bot]"
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+
+exec python -m bump_semver_anywhere $*
